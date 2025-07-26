c7c005a98771850024e93c9dde9a89d353d9bb41
Sandeep Yadav <sandeepyaduvanshi28@gmail.com>
2025-07-26 17:06:13 +0530

    final change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Analytics/>
+        <Analytics />
+        <SpeedInsights />
         {children}
         </body>
     </html>
