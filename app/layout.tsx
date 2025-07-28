bce09bb25209af36b196b9d57e67dac28cbf1065
Sandeep Yadav <sandeepyaduvanshi28@gmail.com>
2025-07-28 17:17:01 +0530

    header update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Header from "@/components/Header";
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -25,6 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <Analytics />
         <SpeedInsights />
         {children}
---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+
+
+
+export default function Header() {
+  return (
+    <header className="bg-white/80 backdrop-blur-md shadow-md sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center">
+        <div className="flex items-center space-x-3">
+          <Image src="/images/icon.png" alt="Logo" width={50} height={50} />
+          <span className="text-3xl font-bold text-purple-700">Brainito</span>
+        </div>
+      </div>
+    </header>
+  );
+}
