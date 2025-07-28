3c84491a852d92e0de860a9a4cc62515a2e8f347
Sandeep Yadav <sandeepyaduvanshi28@gmail.com>
2025-07-28 20:12:29 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,5 +39,6 @@ export default function RootLayout({
         {children}
         </body>
     </html>
+    
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -253,6 +253,7 @@ export default function AITrustScorePage() {
                 </div>
               </CardContent>
             </Card>
+            
 
             {/* What's Included Section */}
             <Card>
