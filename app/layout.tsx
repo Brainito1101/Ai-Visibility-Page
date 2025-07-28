c4eb392b8828051d0d4c900f2861e11751a41f91
Sandeep Yadav <sandeepyaduvanshi28@gmail.com>
2025-07-28 20:20:24 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "@/components/header";
+
 import Head from "next/head"
 
 
@@ -39,6 +40,6 @@ export default function RootLayout({
         {children}
         </body>
     </html>
-    
+
   )
 }
