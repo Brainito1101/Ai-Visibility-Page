e4c63b7215b454c4bb7fb8dfabfe9e66f3ff5bd7
Nidhi Mevada <accounts@brainito.com>
2025-07-31 17:37:09 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className="bg-white/80 backdrop-blur-md shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center">
         <div className="flex items-center space-x-3">
-        <Link href="/" className="flex items-center space-x-3">
+        <Link href="https://www.brainito.com/" className="flex items-center space-x-3">
           <Image src="/images/icon.png" alt="Logo" width={50} height={50} />
           <span className="text-3xl font-bold text-purple-700">Brainito</span>
           </Link>
